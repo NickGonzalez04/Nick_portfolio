14a1ba727959c14f94af8d03327376072501b183
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-02-27 09:11:28 -0500

    Nick_basic_form_created

--- portfolio/src/componet_file.js/contact_form.js
+++ portfolio/src/componet_file.js/contact_form.js
@@ -8,17 +8,29 @@ export default function Contact(){
 
     return(
         <div>
-            <input
-                value=''
-                type='email'
-                placeholder='Email'
-            />
-            <input 
-                value=''
-                type='text'
-                placeholder='Name'
+            <div>
+                <label for='email'>EMAIL: </label>
+                <input
+                    value=''
+                    type='email'
+                    placeholder='Email'
                 />
-            
+            </div>
+            <div>
+                <label for='name'>NAME: </label>
+                <input
+                    value=''
+                    type='text'
+                    placeholder='Name'
+                />
+            </div>
+            <div>
+                <label for='fsubject'>SUBJECT: </label>
+                <textarea
+                    value=''
+                    type='text'
+                    placeholder='Write Something here...' />
+            </div>
         </div>
     )
 }
