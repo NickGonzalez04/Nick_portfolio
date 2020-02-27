b3ab5f325509216e160da27c73c8dc5c75725966
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-02-27 13:03:28 -0500

    Nick_Contact_form_handlesChanges

--- portfolio/src/component_files/04_contact_form.js
+++ portfolio/src/component_files/04_contact_form.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Styles
 import '../style_files/04_contact.scss'
 
-export default function Contact(){
+ const Contact = () => {
+     const [signUp, setSignUp] = useState({
+     });
+
+
+    const handleChange = e => {
+        console.log(e.target.value);
+        setSignUp({
+            ...signUp,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = e => {
+
+        e.preventDefault();
+    }
 
     return(
         <div>
+            <form onSubmit={handleSubmit}>
             <div>
                 <label for='email'>Email: </label>
                 <input
-                    value=''
                     type='email'
                     placeholder='Email'
+                    value={signUp.email}
+                    onChange={handleChange}
                 />
             </div>
             <div>
                 <label for='name'>Name: </label>
                 <input
-                    value=''
                     type='text'
                     placeholder='Name'
+                    value={signUp.user_name}
+                    onChange={handleChange}
                 />
             </div>
             <div>
@@ -30,8 +49,11 @@ export default function Contact(){
                     type='text'
                     placeholder='Message...' />
             </div>
+            </form>
 
             <button type='submit' className='message'>Send Message</button>
         </div>
     )
 };
+
+export default Contact;
