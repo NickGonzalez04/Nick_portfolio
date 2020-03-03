3307e19a4738a05a49122504baa20f3a9990aa53
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-03-03 10:02:21 -0500

    NICK_about_has_icon

--- portfolio/src/component_files/05_about.js
+++ portfolio/src/component_files/05_about.js
@@ -1,5 +1,9 @@
 import React from 'react';
+
+
+// Styles and Icons
 import '../style_files/05_about.scss'
+import {AboutIcon} from '../assests_file/svg_file/00_about_Icon';
 
 
 
@@ -7,6 +11,7 @@ export default function About(){
     return (
         <div className='about'>
             <div>
+            <AboutIcon/>
             <h2>About</h2>
             </div>
             <p>Hi there! My Name is Nicholas Gonzalez and I am software developer. Check out my projects below!</p>
