6f99bdd40079aaacfb377d97963feed258048ab3
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-09-06 01:07:39 -0400

    2.0_04_Project_Area
    - Organized Project area to be more appealing
    - Removed axios for the backend (going to intergrate a new emailer system

--- portfolio/src/component_files/07_project_card.js
+++ portfolio/src/component_files/07_project_card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../style_files/07_project_cards.scss'
+// import '../style_files/07_project_cards.scss'
 import {GitHub} from '../assets_file/svg_file/02_github_icon'
 import { Play } from '../assets_file/svg_file/10_play_buuton';
 
@@ -11,11 +11,12 @@ export default function ProjectCard({ projects }){
     return (
         <div className="card-style">
             <div className="project-image">
-                <a className="photo" href={projects.project_link}>
+                <span className="photo" href={projects.project_link}>
                     <img src={projects.img_src} alt="project image" />
-                </a>
+                </span>
+                <h1>{projects.project_title}</h1>
             </div>
-            <div className="project-info">
+            {/* <div className="project-info">
                 <h1>{projects.project_title}</h1>
                     <p>{projects.description}</p>
                         <br />
@@ -27,16 +28,16 @@ export default function ProjectCard({ projects }){
                         <br />
                         <h3>Back-End Tech Stack:</h3>
                         <p>{projects.tech_stack_2}</p> */}
-                    <a href={projects.link_url}>
-                        <GitHub />
-                    </a>
+                    {/* <button className="buttonLink" href={projects.link_url}>
+                        <GitHub />Github
+                    </button>
                     <a href={projects.project_link}>
                         <img src='/images/Link.png' alt='Link to website' />
                     </a>
                     <a href={projects.marketing_vid}>
                         {projects.marketing_vid ? <Play/> : ''}
                     </a>
-            </div>
+            </div> */}
         </div>
     )
 };
