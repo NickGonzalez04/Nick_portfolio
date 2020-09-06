4e0ba93844945a587b7076a2235e3020832b798e
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-09-05 22:59:18 -0400

    2.0_02_Removed_button_div
    - Removed button div in header.
    - Merged all into one componet.

--- portfolio/src/component_files/02_header_file.js
+++ portfolio/src/component_files/02_header_file.js
@@ -13,12 +13,10 @@ return(
         <div className="section">
             <div className="heading-section">
                 <h1>Hi, I'm Nicholas,
-                    <br></br> Software Engineer living in South Florida ☀️ 🌴 </h1>          
-            </div>
-            <div className='button-section'>
-            <a href="#contact">
-                <button type="submit" className="contact-btn"><h1>Get In Touch</h1></button>
-            </a>
+                    <br></br> Software Engineer living in South Florida ☀️ 🌴 </h1>   
+                    <a href="#contact">
+                <button type="submit" className="contact-btn">Get In Touch</button>
+            </a>       
             </div>
         </div>
     </div>
