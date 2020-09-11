582cb22fb861d2893ed8d3a4d60b7713ebf350ed
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-09-11 17:13:45 -0400

    2.0_14_New_resume_/_footer
    - added updated resume
    - made footer more simple

--- portfolio/src/component_files/09_footer.js
+++ portfolio/src/component_files/09_footer.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import '../style_files/09_footer.scss';
 
-//Icons
-import {Linkedin} from '../assets_file/svg_file/06_linkedin_Icon'
-
-
 export default function Footer(){
-
-
     return (
         <>
         <hr></hr>
 			<div className='footer-nav'>
-				<div className='tabs-container'>
-					<div className='tabs-nav'>
-						<a href='#projects'>Projects</a>
-					</div>
-					<div className='tabs-nav'>
-						<a href='#projects'>Resume</a>
-					</div>
-					<div className='tabs-nav'>
-						<a href='#contact'>Contact</a>
-					</div>
-				</div>
-            
-			<div className='copy-R'>
-                <p>Copyright © {new Date().getFullYear()} Nicholas Gonzalez Built with ☕️ </p>
-			</div>
-               
+				<p>Copyright © {new Date().getFullYear()} Nicholas Gonzalez - Built this with ☕️ and some code.</p>
 			</div>
         </>
 		);
